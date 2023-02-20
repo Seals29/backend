73abc19627c1d7b895298777fe2951cb05a6d2f9
Seals29 <lionelriyadi11@gmail.com>
2023-02-20 22:22:52 +0700

    backend

--- controller/user.go
+++ controller/user.go
@@ -50,6 +50,20 @@ func UpdateUser(c *gin.Context) {
 	config.DB.Save(&user)
 	c.JSON(200, &user)
 }
+func createnewshop(c *gin.Context) {
+	var body struct {
+		Banner []byte
+	}
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body1",
+		})
+		return
+	}
+	c.JSON(200, gin.H{
+		"msg": body,
+	})
+}
 func CreateShop(c *gin.Context) {
 	var body struct {
 		FirstName string
@@ -64,7 +78,14 @@ func CreateShop(c *gin.Context) {
 		})
 		return
 	}
-
+	// gorm.Model
+	// Email       string  `json:"email"`
+	// Name        string  `json:"name"`
+	// Description string  `json:"description"`
+	// Status      string  `json:"status"`
+	// Banner      string  `json:"banner"`
+	// Sales       int     `json:"sales"`
+	// Service     float64 `json:"service"`
 	//hash the pass
 	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
 	if err != nil {
@@ -73,7 +94,15 @@ func CreateShop(c *gin.Context) {
 		})
 		return
 	}
-
+	shop := models.Shop{
+		Email:       body.Email,
+		Name:        body.FirstName,
+		Description: "",
+		IsBan:       false,
+		Banner:      "",
+		Sales:       0,
+		Service:     0.0,
+	}
 	//create the user
 	user := models.User{
 		FirstName: body.FirstName,
@@ -82,6 +111,7 @@ func CreateShop(c *gin.Context) {
 		Email:     body.Email,
 		Password:  string(hash)}
 	config.DB.Create(&user)
+	config.DB.Create(&shop)
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Seller Account Successfuly created",
 		"user":    user,
