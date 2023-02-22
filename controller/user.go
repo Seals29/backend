3f0a8091c30d427c42e1440e98377ac17808a418
Seals29 <lionelriyadi11@gmail.com>
2023-02-22 16:45:42 +0700

    rev1

--- controller/user.go
+++ controller/user.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"fmt"
 	"net/http"
+	"net/mail"
 
 	"github.com/Seals29/config"
 	"github.com/Seals29/models"
@@ -27,10 +28,19 @@ func ValidateUser(c *gin.Context) {
 	})
 
 }
+func EmailValidation(email string) bool {
+	_, err := mail.ParseAddress(email)
+	return err == nil
+}
 func GetUsers(c *gin.Context) {
 	user := []models.User{}
 	config.DB.Find(&user)
 	c.JSON(200, &user)
+}
+func GetShops(c *gin.Context) {
+	shop := []models.Shop{}
+	config.DB.Find(&shop)
+	c.JSON(200, &shop)
 }
 func InsertUser(c *gin.Context) {
 	var user models.User
@@ -72,9 +82,31 @@ func CreateShop(c *gin.Context) {
 		Email     string
 		Password  string
 	}
+
 	if c.Bind(&body) != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": "Failed to read body1",
+		})
+		return
+	}
+	if len(body.FirstName) == 0 {
+		c.JSON(200, gin.H{
+			"error": "Name cannot be empty",
+		})
+		return
+	}
+	if len(body.Email) == 0 || len(body.Password) == 0 {
+		c.JSON(200, gin.H{
+			"error": "Email or Password cannot be empty",
+		})
+		return
+	}
+	email := body.Email
+	if EmailValidation(email) {
+
+	} else {
+		c.JSON(200, gin.H{
+			"error": "Email is not in an email format!",
 		})
 		return
 	}
@@ -117,4 +149,105 @@ func CreateShop(c *gin.Context) {
 		"user":    user,
 	})
 
+}
+func InsertProduct(c *gin.Context) {
+	var body struct {
+		Name        string   `json:"name"`
+		Type        string   `json:"type"`
+		ID          int      `json:"id"`
+		Stock       int      `json:"stock"`
+		Image       []string `json:"image"`
+		Price       int      `json:"price"`
+		Description string   `json:"description"`
+	}
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body1",
+		})
+		return
+	}
+}
+func SetBanUser(c *gin.Context) {
+	var body struct {
+		IsBan bool `json:"isban"`
+		ID    int  `json:"id"`
+	}
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body1",
+		})
+		return
+	}
+	fmt.Println(body)
+	if body.IsBan == false {
+		ban := true
+		var user models.User
+		config.DB.Where("id = ?", body.ID).First(&user)
+		user.IsBan = ban
+		config.DB.Save(&user)
+		c.JSON(200, gin.H{
+			"message": "You have banned " + user.FirstName + user.LastName,
+		})
+		return
+	} else {
+		ban := false
+		var user models.User
+		config.DB.Where("id = ?", body.ID).First(&user)
+		user.IsBan = ban
+		config.DB.Save(&user)
+		c.JSON(200, gin.H{
+			"message": "You have unbanned " + user.FirstName + user.LastName,
+		})
+		return
+	}
+}
+func SetBan(c *gin.Context) {
+	var body struct {
+		IsBan bool   `json:"isban"`
+		Email string `json:"email"`
+	}
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body1",
+		})
+		return
+	}
+	if body.IsBan == false {
+		ban := true
+		var user models.User
+		var shop models.Shop
+		config.DB.Where("email =?", body.Email).First(&user)
+		config.DB.Where("email =?", body.Email).First(&shop)
+		user.IsBan = ban
+		shop.IsBan = ban
+		config.DB.Save(&user)
+		config.DB.Save(&shop)
+		c.JSON(200, gin.H{
+			"message": "You have banned " + user.FirstName + user.LastName,
+		})
+		return
+	} else {
+		ban := false
+		var user models.User
+		var shop models.Shop
+		config.DB.Where("email =?", body.Email).First(&user)
+		config.DB.Where("email =?", body.Email).First(&shop)
+		user.IsBan = ban
+		shop.IsBan = ban
+		config.DB.Save(&user)
+		config.DB.Save(&shop)
+		c.JSON(200, gin.H{
+			"message": "You have banned " + user.FirstName + user.LastName,
+		})
+		return
+	}
+
+	// var user models.User
+	// config.DB.Where("id =?", c.Param("id")).First(&user)
+	// c.BindJSON(&user)
+	// config.DB.Save(&user)
+	// c.JSON(200, &user)
+	// c.JSON(200, gin.H{
+	// 	"message": body,
+	// })
 }
